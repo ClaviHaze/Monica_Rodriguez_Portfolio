a6ad520caa68573e261bdf3c11ca59fef29fcca0
ClaviHaze <60659234+ClaviHaze@users.noreply.github.com>
2020-07-23 03:23:47 -0500

    fixed hero section, added call to action. finished project and contact sections. filled about me section text. Temporarily removed skill section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
+// Get that hamburger menu cookin' //
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener("click", function() {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
 // Smooth Anchor Scrolling
 $(document).on("click", 'a[href^="#"]', function(event) {
   event.preventDefault();
@@ -30,4 +54,12 @@ $(document).ready(function($) {
 $(window).on("load", function() {
   var Body = $("body");
   Body.addClass("preloader-site");
+});
+
+const mywork = document.querySelector('#my-work');
+const heroBtn = document.querySelector('#heroBtn');
+heroBtn.addEventListener('click', () =>  {
+  mywork.scrollIntoView({
+    behavior: 'smooth', inline: 'end',
+  });
 });
